fdaf2ec53e9503ab8388979c2d1d1f14929e5ee0
Jin Gong <gongjin21@hotmail.com>
2023-03-11 10:43:05 +0800

    Create a ios/mac app

---
+++ examples/apple/RustOnApple/RustOnAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustOnAppleApp.swift
+//  RustOnApple
+//
+//  Created by Jin on 2023/3/11.
+//
+
+import SwiftUI
+
+@main
+struct RustOnAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
